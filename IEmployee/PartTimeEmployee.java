231e8863a15890162ca82f5b0af63d6006d42fcb
huuthang2001 <nguyenhuuthang094@gmail.com>
2023-03-05 20:39:01 +0700

    Update PartTimeEmployee.java

---
+++ IEmployee/PartTimeEmployee.java
@@ -0,0 +1,12 @@
+package Java.IEmployee;
+
+public class PartTimeEmployee extends Employee {
+    int WorkingHours;
+    public PartTimeEmployee(String name, int paymentPerHour,int WorkingHours) {
+        super(name, paymentPerHour);
+        this.WorkingHours = WorkingHours;
+    }
+    public int calculateSalary(){
+        return WorkingHours*getPaymentPerHour();
+    }
+}
