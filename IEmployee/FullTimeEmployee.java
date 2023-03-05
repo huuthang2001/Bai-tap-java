6fde59e5c420c4a738d5f9a48304c4985be14aff
huuthang2001 <nguyenhuuthang094@gmail.com>
2023-03-05 20:38:57 +0700

    Create FullTimeEmployee.java

---
+++ IEmployee/FullTimeEmployee.java
@@ -0,0 +1,11 @@
+package Java.IEmployee;
+
+public class FullTimeEmployee extends Employee {
+    public FullTimeEmployee(String name, int paymentPerHour) {
+        super(name, paymentPerHour);
+    }
+    public int calculateSalary(){
+        return 4*40*getPaymentPerHour();
+    }
+    
+}
