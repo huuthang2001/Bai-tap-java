25c8f021760e27876a9f2eaefae120b946725c81
huuthang2001 <nguyenhuuthang094@gmail.com>
2023-03-05 21:37:54 +0700

    Create Circle.java

---
+++ _Pizza/Circle.java
@@ -0,0 +1,15 @@
+package Java._Pizza;
+
+public class Circle implements IShape {
+    double radius;
+    public Circle (double radius){
+        this.radius=radius;
+    }
+    @Override
+    public double getArea(){
+        return 3.14*radius*radius;
+    }
+    public String toString(){
+        return "("+getArea()+" cm^2)";
+    }
+}
