dad2455f7312b2a17ea6a2038400c72e21f86754
huuthang2001 <122240452+huuthang2001@users.noreply.github.com>
2023-03-19 23:00:57 +0700

    Create RedheadDuck.java

---
+++ Duck/RedheadDuck.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class RedheadDuck extends Duck{
+    public RedheadDuck(){
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+    @Override
+    public void display(){
+        System.out.println("Redhead Duck");
+    }
+}
