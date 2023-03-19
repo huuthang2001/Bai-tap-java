a6b09fa12a1bb5755dde4fb3014b38f8a8d3d2bc
huuthang2001 <122240452+huuthang2001@users.noreply.github.com>
2023-03-19 23:00:52 +0700

    Create MallardDuck.java

---
+++ Duck/MallardDuck.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MallardDuck extends Duck {
+    public MallardDuck(){
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+    @Override
+    public void display(){
+        System.out.println("Mallard Duck");
+    }
+}
